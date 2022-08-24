5bec99f5dc0b96cc91acc86a36f5f9c3df047982
Angela Potapchuk <angela.harkov@gmail.com>
2022-08-24 20:18:43 +0300

    Inited SignIn page locators in the SignInPage class

--- src/test/java/page/BasePage.java
+++ src/test/java/page/BasePage.java
@@ -6,102 +6,101 @@ import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
     WebDriver driver;
-        //page header elements
-        @FindBy(css = "#header")
-        WebElement pageHeader;
+    //page header elements
+    @FindBy(css = "#header")
+    WebElement pageHeader;
 
-        @FindBy(css = ".row>a>img")
-        WebElement headerAdv;
+    @FindBy(css = ".row>a>img")
+    WebElement headerAdv;
 
-        @FindBy(xpath = "//*[@class=\"shop-phone\"]")
-        WebElement shopPhone;
+    @FindBy(xpath = "//*[@class=\"shop-phone\"]")
+    WebElement shopPhone;
 
-        @FindBy(css = "span>.icon-phone")
-        WebElement headerPhoneIcn;
+    @FindBy(css = "span>.icon-phone")
+    WebElement headerPhoneIcn;
 
-        @FindBy(id = "contact-link")
-        WebElement contactAsBtn;
+    @FindBy(id = "contact-link")
+    WebElement contactAsBtn;
 
-        @FindBy(css = "a.login")
-        WebElement signInBtn;
+    @FindBy(css = "a.login")
+    WebElement signInBtn;
 
-        @FindBy(xpath = "//div[@id='header_logo']")
-        WebElement headerLogo;
+    @FindBy(xpath = "//div[@id='header_logo']")
+    WebElement headerLogo;
 
-        @FindBy(xpath = "//*[@placeholder=\"Search\"]")
-        WebElement searchFld;
+    @FindBy(xpath = "//*[@placeholder=\"Search\"]")
+    WebElement searchFld;
 
-        @FindBy(xpath = "a[title='View my shopping cart']")
-        WebElement shoppingCartIcn;
+    @FindBy(xpath = "a[title='View my shopping cart']")
+    WebElement shoppingCartIcn;
 
-        @FindBy(xpath = "//*[@id=\"block_top_menu\"]/ul//a[text() = 'Women']")
-        WebElement womanDisplayBlock;
+    @FindBy(xpath = "//*[@id=\"block_top_menu\"]/ul//a[text() = 'Women']")
+    WebElement womanDisplayBlock;
 
-        @FindBy(xpath = "//*[@id=\"block_top_menu\"]/ul/li/a[text() = 'Dresses']")
-        WebElement dressesDisplayBlock;
+    @FindBy(xpath = "//*[@id=\"block_top_menu\"]/ul/li/a[text() = 'Dresses']")
+    WebElement dressesDisplayBlock;
 
-        @FindBy(xpath = "//*[@id=\"block_top_menu\"]/ul/li/a[text() = 'T-shirts']")
-        WebElement tShirtsDisplayBlock;
+    @FindBy(xpath = "//*[@id=\"block_top_menu\"]/ul/li/a[text() = 'T-shirts']")
+    WebElement tShirtsDisplayBlock;
 
-        //page footer elements
-        @FindBy(css = ".footer-container")
-        WebElement pageFooter;
+    //page footer elements
+    @FindBy(css = ".footer-container")
+    WebElement pageFooter;
 
-        @FindBy(xpath = "//*[text() = 'Newsletter']")
-        WebElement NewsletterBlock;
+    @FindBy(xpath = "//*[text() = 'Newsletter']")
+    WebElement NewsletterBlock;
 
-        @FindBy(css = "#newsletter-input")
-        WebElement NewsletterInputFld;
+    @FindBy(css = "#newsletter-input")
+    WebElement NewsletterInputFld;
 
-        @FindBy(xpath = "//*[@value='Enter your e-mail']")
-        WebElement enterYourEmailFldValue;
+    @FindBy(xpath = "//*[@value='Enter your e-mail']")
+    WebElement enterYourEmailFldValue;
 
-        @FindBy(css = "#newsletter_block_left>div>form>div>button")
-        WebElement submitNewsletterBtn;
+    @FindBy(css = "#newsletter_block_left>div>form>div>button")
+    WebElement submitNewsletterBtn;
 
-        @FindBy(css = "#social_block")
-        WebElement footerSocialBlock;
+    @FindBy(css = "#social_block")
+    WebElement footerSocialBlock;
 
-        @FindBy(xpath = "//*[text() = 'Follow us']")
-        WebElement footerFollowUsBlock;
+    @FindBy(xpath = "//*[text() = 'Follow us']")
+    WebElement footerFollowUsBlock;
 
-        @FindBy(css = "li.facebook")
-        WebElement facebookIcn;
+    @FindBy(css = "li.facebook")
+    WebElement facebookIcn;
 
-        @FindBy(css = "li.twitter")
-        WebElement twitterIcn;
+    @FindBy(css = "li.twitter")
+    WebElement twitterIcn;
 
-        @FindBy(css = "li.youtube")
-        WebElement youtubeIcn;
+    @FindBy(css = "li.youtube")
+    WebElement youtubeIcn;
 
-        @FindBy(css = "li.google-plus")
-        WebElement googlePlusIcn;
+    @FindBy(css = "li.google-plus")
+    WebElement googlePlusIcn;
 
-        @FindBy(xpath = "//h4[text()='Categories']")
-        WebElement CategoriesSection;
+    @FindBy(xpath = "//h4[text()='Categories']")
+    WebElement CategoriesSection;
 
-        @FindBy(xpath = "//h4[text()='Information']")
-        WebElement informationSection;
+    @FindBy(xpath = "//h4[text()='Information']")
+    WebElement informationSection;
 
-        @FindBy(xpath = "//h4/a[text()='My account']")
-        WebElement myAccountSection;
+    @FindBy(xpath = "//h4/a[text()='My account']")
+    WebElement myAccountSection;
 
-        @FindBy(xpath = "//h4[text()='Store information']")
-        WebElement storeInformationSection;
+    @FindBy(xpath = "//h4[text()='Store information']")
+    WebElement storeInformationSection;
 
-        @FindBy(css = ".icon-map-marker")
-        WebElement mapMarkerIcn;
+    @FindBy(css = ".icon-map-marker")
+    WebElement mapMarkerIcn;
 
-        @FindBy(css = "li>.icon-phone")
-        WebElement footerPhoneIcn;
+    @FindBy(css = "li>.icon-phone")
+    WebElement footerPhoneIcn;
 
-        @FindBy(css = ".icon-envelope-alt")
-        WebElement envelopeIcn;
+    @FindBy(css = ".icon-envelope-alt")
+    WebElement envelopeIcn;
 
-        public BasePage(WebDriver driver) {
-            PageFactory.initElements(driver, this);
-            this.driver = driver;
-            driver.get("https://automationpractice.com/index.php");
-        }
-
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        driver.get("https://automationpractice.com/index.php");
+        PageFactory.initElements(driver, this);
+    }
 }
--- src/test/java/page/SignInPage.java
+++ src/test/java/page/SignInPage.java
@@ -1,4 +1,52 @@
 package page;
 
-public class SignInPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignInPage extends BasePage {
+
+    public SignInPage(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+        driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(xpath = "//*[@id='center_column']/h1")
+    WebElement authenticationPageHeader;
+
+    @FindBy(xpath = "//*[@id='create-account_form']/h3")
+    WebElement createAnAccountFormHeader;
+
+    @FindBy(xpath = "//*[@id='email_create']")
+    WebElement createAccountEmailField;
+
+    @FindBy(xpath = "//*[text() = 'Create an account']")
+//    @FindBy(xpath = "//button[@id='SubmitCreate']/span")
+    WebElement createAccountSubmitBtn;
+
+    @FindBy(xpath = "//*[@id='login_form']/h3")
+    WebElement alreadyRegisteredFormHeader;
+
+    @FindBy(xpath = "//*[@id='email']")
+    WebElement emailAddressField;
+
+    @FindBy(xpath = "//*[@id='passwd']")
+    WebElement passwordField;
+
+    @FindBy(css = "a[title='Recover your forgotten password']")
+//    @FindBy(xpath = "//a[title='Recover your forgotten password']")
+    WebElement forgotPasswordLink;
+
+    @FindBy(css = "button#SubmitLogin")
+//    @FindBy(xpath = "//*[text() = 'Sign in']")
+//    @FindBy(xpath = "//button[@id='SubmitLogin']")
+    WebElement SignInBtn;
+
+    @FindBy(css = ".icon-home")
+    WebElement homeIcn;
+
+    @FindBy(xpath = "//span[@class='navigation_page']")
+    WebElement navigationPipe;
 }
