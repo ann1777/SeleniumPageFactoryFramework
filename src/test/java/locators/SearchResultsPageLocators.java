91592a5580e059918816c6d7cf7465cbbc835c69
Angela Potapchuk <angela.harkov@gmail.com>
2022-08-30 18:24:15 +0300

    Locators for SearchResults page have been collected in SearchResultsPage_OR class and have been initialized in SearchResultsPageLocators class

--- src/test/java/locators/SearchResultsPageLocators.java
+++ src/test/java/locators/SearchResultsPageLocators.java
@@ -105,11 +105,165 @@ public class SearchResultsPageLocators extends BasePageLocators{
     @FindBy(css = SearchResultsPageLocators_OR.FOUNNDEDRESULTS_FIRSTPRODUCT_BOTTOM_COMPARE_BTN)
     WebElement firstProductBottomCompareBtn;
 
+    @FindBy(css = SearchResultsPageLocators_OR.TOP_SELLERS_LEFT_BLOCK)
+    WebElement blockRightTopSellers;
 
+    @FindBy(css = SearchResultsPageLocators_OR.TOP_SELLERS_LEFT_BLOCK_TITLE)
+    WebElement blockRightTopSellersTitle;
+
+    @FindBy(css = SearchResultsPageLocators_OR.TOP_SELLERS_LEFT_BLOCK_CONTENT)
+    WebElement blockRightTopSellersContent;
+
+    @FindBy(css = SearchResultsPageLocators_OR.TOP_SELLERS_BLOCK_CONTENT_FIRST_ITEM)
+    WebElement blockRightTopSellersFirstItem;
+
+    @FindBy(css = SearchResultsPageLocators_OR.TOP_SELLERS_BLOCK_CONTENT_FIRST_ITEM_IMG)
+    WebElement blockRightTopSellersFirstItemImg;
+
+    @FindBy(css = SearchResultsPageLocators_OR.TOP_SELLERS_BLOCK_CONTENT_FIRST_ITEM_NAME)
+    WebElement blockRightTopSellersFirstItemNama;
+
+    @FindBy(css = SearchResultsPageLocators_OR.TOP_SELLERS_BLOCK_CONTENT_FIRST_ITEM_DESCRIPTION)
+    WebElement blockRightTopSellersFirstItemDesc;
+
+    @FindBy(css = SearchResultsPageLocators_OR.TOP_SELLERS_BLOCK_CONTENT_FIRST_ITEM_PRICE)
+    WebElement blockRightTopSellersFirstItemPrice;
+
+    @FindBy(css = SearchResultsPageLocators_OR.ALL_BEST_SELLERS_BTN)
+    WebElement allBestSellersBtn;
+
+    @FindBy(css = SearchResultsPageLocators_OR.ALL_BEST_SELLERS_BTN_RIGHT_ICN)
+    WebElement allBestSellersBtnIcn;
+
+    @FindBy(css = SearchResultsPageLocators_OR.CATEGORIES_LEFT_BLOCK)
+    WebElement categories;
+
+    @FindBy(css = SearchResultsPageLocators_OR.CATEGORIES_LEFT_BLOCK_TITLE)
+    WebElement categoriesLeftBlock;
+
+    @FindBy(css = SearchResultsPageLocators_OR.CATEGORIES_LEFT_BLOCK_CONTENT)
+    WebElement categoriesLeftBlockContent;
+
+    @FindBy(css = SearchResultsPageLocators_OR.CATEGORIES_LEFT_BLOCK_CONTENT_PLUS_BTN)
+    WebElement categoriesLeftBlockContentPlusBtn;
+
+    @FindBy(css = SearchResultsPageLocators_OR.CATEGORIES_LEFT_BLOCK_CONTENT_TOP)
+    WebElement categoriesLeftBlockContentTop;
+
+    @FindBy(css = SearchResultsPageLocators_OR.CATEGORIES_LEFT_BLOCK_CONTENT_TOP_PLUS_BTN)
+    WebElement categoriesLeftBlockContentTopPlusBtn;
+
+    @FindBy(css = SearchResultsPageLocators_OR.INFORMATION_LEFT_BLOCK)
+    WebElement informationLeftBlock;
+
+    @FindBy(css = SearchResultsPageLocators_OR.INFORMATION_LEFT_BLOCK_TITLE)
+    WebElement informationLeftBlockTitle;
+
+    @FindBy(css = SearchResultsPageLocators_OR.INFORMATION_LEFT_BLOCK_CONTENT)
+    WebElement informationLeftBlockContent;
+
+    @FindBy(css = SearchResultsPageLocators_OR.INFORMATION_LEFT_BLOCK_CONTENT_PLUS_ABOUT_US_SUBCATEGORY)
+    WebElement informationLeftBlockContentAboutAs;
+
+    @FindBy(css = SearchResultsPageLocators_OR.MANUFACTURES_LEFT_BLOCK)
+    WebElement manufacturesLeftBlock;
+
+    @FindBy(css = SearchResultsPageLocators_OR.MANUFACTURES_LEFT_BLOCK_TITLE)
+    WebElement manufacturesLeftBlockTitle;
+
+    @FindBy(css = SearchResultsPageLocators_OR.MANUFACTURES_LEFT_BLOCK_CONTENT)
+    WebElement manufacturesLeftBlockContent;
+
+    @FindBy(css = SearchResultsPageLocators_OR.MANUFACTURES_LEFT_BLOCK_FASHION)
+    WebElement manufacturesLeftBlockFashion;
+
+    @FindBy(css = SearchResultsPageLocators_OR.MANUFACTURES_LEFT_BLOCK_FASHION_LINK)
+    WebElement manufacturesLeftBlockFashionLink;
+
+    @FindBy(css = SearchResultsPageLocators_OR.MANUFACTURES_LEFT_BLOCK_ALL_LIST)
+    WebElement manufacturesLeftBlockAllList;
+
+    @FindBy(css = SearchResultsPageLocators_OR.MANUFACTURES_LEFT_BLOCK_ALL_LIST_FASHION_OPTION)
+    WebElement manufacturesLeftBlockAllListFashionOption;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SPECIALS_LEFT_BLOCK)
+    WebElement specialsLeftBlock;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SPECIALS_LEFT_BLOCK_TITLE)
+    WebElement specialsLeftBlockTitle;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SPECIALS_LEFT_BLOCK_CONTENT)
+    WebElement specialsLeftBlockContent;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SPECIALS_LEFT_BLOCK_PRODUCT_IMG)
+    WebElement specialsLeftBlockProductIcn;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SPECIALS_LEFT_BLOCK_PRODUCT_CONTENT)
+    WebElement specialsLeftBlockProductContent;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SPECIALS_LEFT_BLOCK_PRODUCT_PRICE)
+    WebElement specialsLeftBlockProductPrice;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SPECIALS_LEFT_BLOCK_ALL_SPECIALS_BTN)
+    WebElement specialsLeftBlockAllSpecialsBtn;
+
+    @FindBy(css = SearchResultsPageLocators_OR.OUR_STORES_LEFT_BLOCK)
+    WebElement ourStoresLeftBlock;
+
+    @FindBy(css = SearchResultsPageLocators_OR.OUR_STORES_LEFT_BLOCK_TITLE)
+    WebElement ourStoresLeftBlockTitle;
+
+    @FindBy(css = SearchResultsPageLocators_OR.OUR_STORES_LEFT_BLOCK_CONTENT)
+    WebElement ourStoresLeftBlockContent;
+
+    @FindBy(css = SearchResultsPageLocators_OR.OUR_STORES_LEFT_BLOCK_PRODUCT_IMG)
+    WebElement ourStoresLeftBlockProductImg;
+
+    @FindBy(css = SearchResultsPageLocators_OR.OUR_STORES_LEFT_BLOCK_PRODUCT_DISCOVER_BTN)
+    WebElement ourStoresLeftBlockProductDiscoverBtn;
+
+    @FindBy(css = SearchResultsPageLocators_OR.OUR_STORES_LEFT_BLOCK_PRODUCT_DISCOVER_BTN_ICN)
+    WebElement ourStoresLeftBlockProductDiscoverBtnIcn;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SUPPLIERS_LEFT_BLOCK)
+    WebElement suppliersLeftBlock;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SUPPLIERS_LEFT_BLOCK_TITLE)
+    WebElement suppliersLeftBlockTitle;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SUPPLIERS_LEFT_BLOCK_CONTENT)
+    WebElement suppliersLeftBlockContent;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SUPPLIERS_LEFT_BLOCK_FASHION_SUPPLIER)
+    WebElement suppliersLeftBlockFashionSuppliers;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SUPPLIERS_LEFT_BLOCK_ALL_SUPPLIERS_DROPDOWN)
+    WebElement suppliersLeftBlockAllSuppliersDropdown;
+
+    @FindBy(css = SearchResultsPageLocators_OR.SUPPLIERS_LEFT_BLOCK_ALL_SUPPLIERS_OPTION)
+    WebElement suppliersLeftBlockAllSuppliersOption;
+
+    @FindBy(css = SearchResultsPageLocators_OR.VIEWED_PRODUCTS_LEFT_BLOCK)
+    WebElement viewedProductsLeftBlock;
+
+    @FindBy(css = SearchResultsPageLocators_OR.VIEWED_PRODUCTS_LEFT_BLOCK_TITLE)
+    WebElement viewedProductsLeftBlockTitle;
+
+    @FindBy(css = SearchResultsPageLocators_OR.VIEWED_PRODUCTS_LEFT_BLOCK_CONTENT)
+    WebElement viewedProductsLeftBlockContent;
+
+    @FindBy(css = SearchResultsPageLocators_OR.VIEWED_PRODUCTS_LEFT_BLOCK_LAST_VIEWED_ITEM)
+    WebElement viewedProductsLeftBlockLastViewedItem;
+
+    @FindBy(css = SearchResultsPageLocators_OR.VIEWED_PRODUCTS_LEFT_BLOCK_LAST_VIEWED_ITEM_IMG)
+    WebElement viewedProductsLeftBlockLastViewedItemImg;
+
+    @FindBy(css = SearchResultsPageLocators_OR.VIEWED_PRODUCTS_LEFT_BLOCK_LAST_VIEWED_ITEM_DESC)
+    WebElement viewedProductsLeftBlockLastViewedItemImgDesc;
 
     public SearchResultsPageLocators(WebDriver driver) {
         this.driver = driver;
-        driver.get("https://automationpractice.com/index.php");
+        driver.get("http://automationpractice.com/index.php?controller=search&orderby=position&orderway=desc&search_query=Dress&submit_search=");
         PageFactory.initElements((ElementLocatorFactory) driver, this);
     }
     public SearchResultsPageLocators() {
