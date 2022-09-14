bfec1d982abe90249566e92b1e875d61be0d0b72
Angela Potapchuk <angela.harkov@gmail.com>
2022-09-14 20:05:55 +0300

    Modified PropertyReader class and driver-config.properties

--- src/test/java/helpers/browserConfig/PropertyReader.java
+++ src/test/java/helpers/browserConfig/PropertyReader.java
@@ -1,10 +1,9 @@
 package helpers.browserConfig;
 
 import helpers.resource.ResourceHelper;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.*;
@@ -14,125 +13,68 @@ public abstract class PropertyReader implements ConfigReader {
 	private static FileInputStream file;
 	public static Properties OR;
 
-	public PropertyReader() throws IOException {
+	public String PropertyReader() throws IOException {
+
+		Map<String, String> properties = Map.of(
+				"chrome_binary", "src/test/resources/drivers/chromedriver.exe",
+				"load_timeout_sec", "50",
+				"id", "qa",
+				"website_host", "https://automationpractice.com/index.php",
+				"user_name_pref", "user",
+				"email_suf", "@gmail.com",
+				"browser_type", "Chrome",
+				"implicit_wait", "500",
+				"explicit_wait", "visibilityOfElementLocated"
+		);
+
+		PropertyReader instance = new PropertyReader() {
+			@Contract(pure = true)
+			@Override
+			public @Nullable Object reader(Object param) {
+				return null;
+			}
+		};
 
 		file = new FileInputStream(
-				new Map<String, String>(ResourceHelper.getResourcePath("src/test/resources/configfiles/driver-config.properties")) {
-					@Override
-					public int size() {
-						return this.size();
-					}
+				new Map<String, String> OR = (Properties) instance.reader(ResourceHelper.getResourcePath("src/test/resources/configfiles/driver-config.properties")) {
+		};
 
-					@Override
-					public boolean isEmpty() {
-						if(this.size() == 0) return true;
-						return false;
-					}
+		public String getImplicitWait() {
+			List<String> implicitWait = List.of(OR.getProperty("implicit_wait"));
+			return String.valueOf(implicitWait);
+		}
 
-					@Override
-					public boolean containsKey(Object key) {
-						if(this.keySet().contains(key)) return true;
-						return false;
-					}
+		public String getExplicitWait() {
+			List<String> explicitWait = List.of(OR.getProperty("explicit_wait"));
+			return String.valueOf(explicitWait);
+		}
 
-					@Override
-					public boolean containsValue(Object value) {
-						if(this.containsValue(value)) return true;
-						return false;
-					}
+		public String getPageLoadTimeOut() {
+			List<String> loadTimeOut = List.of(OR.getProperty("load_timeout_sec"));
+			return String.valueOf(loadTimeOut);
+		}
 
-					@Override
-					public String get(Object key) {
-						Arrays.stream(this.keySet().toArray()).iterator();
-						return (String) key;
-					}
+		public String getBrowserType() {
+			List<String> browserType = List.of(OR.getProperty("browser_type"));
+			return String.valueOf(browserType); }
 
-//					@Nullable
-//					@Override
-//					public String put(String key, String value)
-//
-//						return new Map<>;
-//					}
-//
-//					@Override
-//					public String remove(Object key) {
-//						return null;
-//					}
-//
-//					@Override
-//					public void putAll(@NotNull Map<? extends String, ? extends String> m) {
-//
-//					}
-//
-//					@Override
-//					public void clear() {
-//
-//					}
-//
-//					@NotNull
-//					@Override
-//					public Set<String> keySet() {
-//						return null;
-//					}
-//
-//					@NotNull
-//					@Override
-//					public Collection<String> values() {
-//						return null;
-//					}
-//
-//					@NotNull
-//					@Override
-//					public Set<Entry<String, String>> entrySet() {
-//						return null;
-//					}
-//
-//					@NotNull
-//					@Override
-//					public Object[] toArray() {
-//						return new Object[new Properties];
-//					}
-				});
-		OR = new Properties();
-		OR.load(file);
+		public String getBaseUrl() {
+			List<String> baseUrl = List.of(OR.getProperty("website_host"));
+			return String.valueOf(baseUrl); }
 
-	}
+		public String getName() {
+			List<String> userName = List.of(OR.getProperty("user_name"));
+			return String.valueOf(userName);
+		}
 
-	public int getImplicitWait() {
-		List<String> implicitWait = List.of(OR.getProperty("implicit_wait"));
-		return Integer.parseInt(String.valueOf(implicitWait));
-	}
+		public String getEmail() {
+			List<String> userEmail = List.of(OR.getProperty("user_email"));
+			return String.valueOf(userEmail);
+		}
 
-	public String getExplicitWait() {
-		List<String> explicitWait = List.of(OR.getProperty("explicit_wait"));
-		return String.valueOf(explicitWait);
-	}
-
-	public int getPageLoadTimeOut() {
-		List<String> loadTimeOut = List.of(OR.getProperty("load_timeout_sec"));
-		return Integer.parseInt(String.valueOf(loadTimeOut));
-	}
-
-	public String getBrowserType() {
-		List<String> browserType = List.of(OR.getProperty("browser_type"));
-		return String.valueOf(browserType); }
-
-	public String getBaseUrl() {
-		List<String> baseUrl = List.of(OR.getProperty("website_host"));
-		return String.valueOf(baseUrl); }
-
-	public String getName() {
-		List<String> userName = List.of(OR.getProperty("user_name"));
-		return String.valueOf(userName);
-	}
-
-	public String getEmail() {
-		List<String> userEmail = List.of(OR.getProperty("user_email"));
-		return String.valueOf(userEmail);
-	}
-
-	public String getBrowserBinary() {
-		List<String> chromeExe = List.of(OR.getProperty("chrome_binary"));
-		return String.valueOf(chromeExe);
+		public String getBrowserBinary() {
+			List<String> chromeExe = List.of(OR.getProperty("chrome_binary"));
+			return String.valueOf(chromeExe);
+		}
 	}
 }
